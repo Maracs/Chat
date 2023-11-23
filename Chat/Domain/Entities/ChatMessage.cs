0711882bb14ef66b39912bcd4340e771b9d27415
Maracs <lisovskiy0391@gmail.com>
2023-11-23 21:55:25 +0300

    Added Entities

---
+++ Chat/Domain/Entities/ChatMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ChatMessage
+    {
+        public int ChatId { get; set; }
+        public int MessageId {get; set; }
+        public DateTime SendTime { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Chat/Domain/Entities/ChatUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ChatUser
+    {
+        public int ChatId { get; set; }
+        public int UserId { get; set; }
+        public DateTime JoinTime { get; set; }
+    }
+}
