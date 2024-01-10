9408a569877082e704385d098c22bb8c35237487
Maracs <lisovskiy0391@gmail.com>
2024-01-10 22:11:26 +0300

    Added PresentationLayer

---
+++ Settings/PresentationLayer/Controllers/SettingsController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLayer.Contracts;
+using BusinessLayer.Dtos;
+using BusinessLayer.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    [Route("api/settings")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private readonly ISettingsService _settingsService;
+
+        public SettingsController(ISettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<List<SettingDto>>> GetSettingAsync(CancellationToken token)
+        {
+            var userId = User.GetUserId();
+
+            return Ok(await _settingsService.GetSettingAsync(userId, token));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult> AddSettingAsync([FromBody] SettingDto settingDto,CancellationToken token)
+        {
+            var userId = User.GetUserId();
+            await _settingsService.AddSettingAsync(settingDto,userId, token);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Authorize]
+        public async Task<ActionResult> UpdateSettingAsync([FromBody] SettingDto settingDto, CancellationToken token)
+        {
+            var userId = User.GetUserId();
+            await _settingsService.UpdateSettingAsync(settingDto,userId, token);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<ActionResult> DeleteSettingAsync(CancellationToken token)
+        {
+            var userId = User.GetUserId();
+            await _settingsService.DeleteSettingAsync(userId, token);
+
+            return Ok();
+        }
+    }
+}
---
+++ Settings/PresentationLayer/Program.cs
@@ -0,0 +1,49 @@
+using BusinessLayer.AutoMapperProfile;
+using BusinessLayer.Contracts;
+using BusinessLayer.Extensions;
+using BusinessLayer.Services;
+using DataAccessLayer.Extensions;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped(typeof(CancellationToken), serviceProvider =>
+{
+    IHttpContextAccessor httpContext = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+    return httpContext.HttpContext?.RequestAborted ?? CancellationToken.None;
+});
+builder.Services.ConfigureMongoDb(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(SettingsProfile).Assembly);
+builder.Services.AddIdentityService(builder.Configuration);
+builder.Services.AddSwaggerService();
+builder.Services.AddScoped<ISettingsService,SettingsService>();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseExceptionHandlerMiddleware();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
