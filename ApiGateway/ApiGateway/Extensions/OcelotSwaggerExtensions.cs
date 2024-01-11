49a8d9472b2bd7b55315cc8ff2434b32a68c2cec
Maracs <lisovskiy0391@gmail.com>
2024-01-11 21:10:25 +0300

    Added project extensions

---
+++ ApiGateway/ApiGateway/Extensions/OcelotSwaggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiGateway.Extensions
+{
+    public static class OcelotSwaggerExtensions
+    {
+        public static void UseOcelotSwagger(this IApplicationBuilder app, IConfiguration config)
+        {
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = config["Ocelot:PathToSwaggerGen"];
+            });
+        }
+    }
+}
