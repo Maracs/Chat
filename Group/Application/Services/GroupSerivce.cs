15edce0ff08342fc89e3de8138981066644bbdb2
Maracs <lisovskiy0391@gmail.com>
2024-01-15 01:12:01 +0300

    Added Logging to Group

--- Group/Application/Services/GroupSerivce.cs
+++ Group/Application/Services/GroupSerivce.cs
@@ -4,6 +4,7 @@ using Application.Ports.Services;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Interfaces;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Application.Services
@@ -12,15 +13,21 @@ namespace Application.Services
     {
         private readonly IGroupRepository _groupRepository;
         private readonly IMapper _mapper;
+        private readonly ILogger<GroupService> _logger;
 
-        public GroupService(IGroupRepository groupRepository, IMapper mapper)
+        public GroupService(IGroupRepository groupRepository, IMapper mapper, ILogger<GroupService> logger)
         {
             _groupRepository = groupRepository;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public async Task CreateAsync(int userId, CreateGroupDto groupDto, CancellationToken token)
         {
+            _logger.LogInformation(
+              "Trying to call CreateAsync."
+            );
+
             if (userId != groupDto.CreatorId)
             {
                 throw new ApiException("Invalid operation", ExceptionStatus.BadRequest);
@@ -29,10 +36,16 @@ namespace Application.Services
             var group = _mapper.Map<Group>(groupDto);
             await _groupRepository.CreateAsync(group);
             await _groupRepository.SaveChangesAsync(token);
+
+            _logger.LogInformation(
+                 "CreateAsync was called successfully."
+               );
         }
 
         public async Task DeleteAsync(int userId, int id, CancellationToken token)
         {
+            _logger.LogInformation("Trying to call DeleteAsync.");
+
             var group = await _groupRepository.GetByIdAsync(id,token);
 
             if(group is null)
@@ -48,15 +61,25 @@ namespace Application.Services
 
             _groupRepository.Delete(id);
             await _groupRepository.SaveChangesAsync(token);
+
+            _logger.LogInformation("DeleteAsync was called successfully.");
         }
 
         public async Task<List<GroupDto>> GetAllAsync(int userId, int offset, int limit, CancellationToken token)
         {
-            return _mapper.Map<List<GroupDto>>(await _groupRepository.GetAllAsync(userId, offset, limit, token));
+            _logger.LogInformation("Trying to call GetAllAsync.");
+
+            var groups = _mapper.Map<List<GroupDto>>(await _groupRepository.GetAllAsync(userId, offset, limit, token));
+
+            _logger.LogInformation("GetAllAsync was called successfully.");
+
+            return groups;
         }
 
         public async Task<GroupDto> GetByIdAsync(int userId, int id, CancellationToken token)
         {
+            _logger.LogInformation("Trying to call GetByIdAsync.");
+
             var group = await _groupRepository.GetByIdAsync(id,token);
 
             if (group is null)
@@ -69,11 +92,15 @@ namespace Application.Services
                 throw new ApiException("Invalid operation", ExceptionStatus.BadRequest);
             };
 
+            _logger.LogInformation("GetByIdAsync was called successfully.");
+
             return _mapper.Map<GroupDto>(group);
         }
 
         public async Task UpdateAsync(int userId, int id, CreateGroupDto groupDto, CancellationToken token)
         {
+            _logger.LogInformation("Trying to call UpdateAsync.");
+
             var group = await _groupRepository.GetByIdAsync(id,token);
 
             if (group is null)
@@ -89,6 +116,8 @@ namespace Application.Services
             _mapper.Map<CreateGroupDto, Group>(groupDto, group);
             _groupRepository.Update(group);
             await _groupRepository.SaveChangesAsync(token);
+
+            _logger.LogInformation("UpdateAsync was called successfully.");
         }
     }
 }
--- Group/Application/Services/PostService.cs
+++ Group/Application/Services/PostService.cs
@@ -4,6 +4,7 @@ using Application.Ports.Services;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Interfaces;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Application.Services
@@ -13,16 +14,20 @@ namespace Application.Services
         private readonly IPostRepository _postRepository;
         private readonly IGroupRepository _groupRepository;
         private readonly IMapper _mapper;
+        private readonly ILogger<PostService> _logger;
 
-        public PostService(IPostRepository postRepository, IGroupRepository groupRepository, IMapper mapper)
+        public PostService(IPostRepository postRepository, IGroupRepository groupRepository, IMapper mapper, ILogger<PostService> logger)
         {
             _postRepository = postRepository;
             _groupRepository = groupRepository;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public async Task DeleteAsync(int userId, int groupId, int id, CancellationToken token)
         {
+            _logger.LogInformation("Trying to call DeleteAsync.");
+
             var group = await _groupRepository.GetByIdAsync(groupId,token);
 
             if (group is null)
@@ -41,17 +46,25 @@ namespace Application.Services
 
             _postRepository.Delete(groupId, id);
             await _postRepository.SaveChangesAsync(token);
+
+            _logger.LogInformation("DeleteAsync was called successfully.");
         }
 
         public async Task<List<PostDto>> GetAllAsync(int userId, int chatid, int offset, int limit, CancellationToken token)
         {
+            _logger.LogInformation("Trying to call GetAllAsync.");
+
             var posts = await _postRepository.GetAllAsync(userId, chatid, offset, limit,token);
+
+            _logger.LogInformation("GetAllAsync was called successfully.");
 
             return _mapper.Map<List<PostDto>>(posts);
         }
 
         public async Task SendAsync(int userId, PostDto postDto, CancellationToken token)
         {
+            _logger.LogInformation("Trying to call SendAsync.");
+
             var chat = await _groupRepository.GetByIdAsync(postDto.GroupId,token);
 
             if (chat is null)
@@ -68,10 +81,14 @@ namespace Application.Services
 
             await _postRepository.SendAsync(post, postDto.Photos);
             await _postRepository.SaveChangesAsync(token);
+
+            _logger.LogInformation("SendAsync was called successfully.");
         }
 
         public async Task UpdateAsync(int userId, int groupId, int id, string content, CancellationToken token)
         {
+            _logger.LogInformation("Trying to call UpdateAsync.");
+
             var group = await _groupRepository.GetByIdAsync(groupId,token);
 
             if (group is null)
@@ -92,6 +109,8 @@ namespace Application.Services
             post.Content = content;
             _postRepository.Update(post);
             await _postRepository.SaveChangesAsync(token);
+
+            _logger.LogInformation("UpdateAsync was called successfully.");
         }
     }
 }
